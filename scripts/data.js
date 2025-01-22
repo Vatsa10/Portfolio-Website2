8bd2e0f32a88c18466eb4f698805cc966d488424
Vatsa Joshi <123623531+Vatsa10@users.noreply.github.com>
2025-01-22 23:46:45 +0530

    Create data.js

---
+++ scripts/data.js
@@ -0,0 +1,86 @@
+const linkedin = "https://www.linkedin.com/in/vatsa-joshi/";
+const github = "https://github.com/Vatsa10/";
+const email = "mailto:vatsajoshi2@gmail.com";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey there! 👋 I'm Vatsa Joshi.",
+  "<br>",
+  "Welcome to my digital space!",
+  "<br>",
+  "I'm currently pursuing a Bachelor of Technology (BTech) in Computer Science at Navrachana University, Vadodara.",
+  "I have a passion for creating solutions that are not just innovative but also meaningful—things that make a real difference in people's lives.",
+  "<br>",
+  "Curiosity drives me to explore new domains, embrace challenges, and dive into quirky, out-of-the-box projects.",
+  "Collaboration and constant learning are at the core of my journey as I aim to grow both personally and professionally.",
+  "<br>",
+  "This portfolio is a reflection of my journey, ideas, and experiments.",
+  "I hope you enjoy exploring my work as much as I enjoyed creating it.",
+  "<br>",
+  "Let’s build something amazing together! 🚀",
+  "<br>",
+]
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Vatsa Joshi</span>',
+  "<br>",
+  "██╗   ██╗  █████╗ ████████╗ ███████╗  █████╗",
+  "██║   ██║ ██╔══██╗╚══██╔══╝ ██╔════╝ ██╔══██╗",
+  "██║   ██║ ███████║   ██║    ███████╗ ███████║",
+  "██║   ██║ ██╔══██║   ██║    ╚════██║ ██╔══██║",
+  "╚██████╔╝ ██║  ██║   ██║    ███████║ ██║  ██║",
+
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/vatsa-joshi' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/Vatsa10' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command">1. Event Management and Ticketing Android App</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Online Food Ordering System',
+  "<br>",
+  "<br>",
+  '<span class="command">3. Youtube Video Summarizer',
+  "<br>",
+  "<br>",
+  '<span class="command">4. Stonks: Stock Prediction and Visualisation </span>',
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/Vatsa10/" target="_blank">(Github)</a>',
+  "<br>",
+];
